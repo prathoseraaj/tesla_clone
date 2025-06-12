8d5815b42f92da2ec9d70f25198a4acae7ecfe6d
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-06-12 17:12:22 +0530

    feat: details of the products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const Product = () => {
+  const product = [
+    {
+      id: 1,
+      name: "Solar Panels",
+      description: "Use Solar Energy to Power Your Home and Charge Your Tesla",
+      image:
+        "https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Card-Solar-Panels-Desktop.png",
+    },
+    {
+      id: 2,
+      name: "Powerwall",
+      description: "Keep Your Lights On During Outages",
+      image:
+        "https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Card-Powerwall-Desktop.png",
+    },
+    {
+      id: 3,
+      name: "Solar Roof",
+      description: "Generate Clean Energy With Your Roof",
+      image:
+        "https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Card-Solar-Roof-Desktop.png",
+    },
+    {
+      id: 4,
+      name: "Megapack",
+      description: "Massive Batteries for Massive Energy Support",
+      image:
+        "https://digitalassets.tesla.com/tesla-contents/image/upload/f_auto,q_auto/Homepage-Card-Megapack-Desktop.jpg",
+    },
+  ];
   return (
-    <div>Product</div>
-  )
-}
+    <div className="m-10">
+      <div></div>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
